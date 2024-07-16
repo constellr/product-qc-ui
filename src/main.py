1c459d1851ae727f0f861fb86ae8357478bc62c5
Jonas Berhin <jonas.berhin@constellr.com>
2024-07-16 15:25:46 +0200

    cleaning

--- src/main.py
+++ src/main.py
@@ -1,256 +1,89 @@
+import random
+import pandas as pd
 import streamlit as st
-from streamlit_folium import st_folium
-import geopandas as gpd
-import fsspec
-from datetime import datetime, timedelta, date
-import re
+import matplotlib.pyplot as plt
+from glob import glob
+import json
+import base64
+import altair as alt
 
 
+def open_image(path: str):
+    with open(path, "rb") as p:
+        file = p.read()
+        return f"data:image/png;base64,{base64.b64encode(file).decode()}"
 
 
-fields = list_fields()
-customers = sorted([i["name"] for i in fields['Organisation']["options"]])
-today = datetime.now()
-previous_2_weeks = today - timedelta(weeks=2)
-next_1_months = today + timedelta(weeks=4)
-next_6_months = today + timedelta(weeks=26)
+def parse_json(path):
+    metadata = json.load(open(f"{path}/metadata.json"))
+    metadata["scene_datetime"] = pd.Timestamp(metadata["scene_datetime"])
+    metadata["scene_date"] = pd.Timestamp(metadata["scene_datetime"]).normalize()
+    metadata["valid_pixel"] = 100 - metadata["cloud_cover"] - metadata["no_data"]
+    metadata["rgb"] = open_image(f"{path}/rgb.png")
+    metadata["lst"] = open_image(f"{path}/lst.png")
+    metadata["count"] = 1
+    return metadata
+
+@st.cache_data
+def load():
+    df = pd.DataFrame([parse_json(d) for d in glob("tests/fixtures/*")])
+    return df
+df = load()
 
 
-form_default = {
-    "organisation": None,
-    "contact": "",
-    "contract_type": 0,
-    "contract_status": 0,
-    "order_type": 0,
-    "paid_order": 0,
-    "constellr_contact": "",
-    "requested_order": 'Single Order',
-    "requested_period": (previous_2_weeks, today),
-    "requested_delivery": next_1_months,
-    "delivery_method": 0,
-    "comment": "",
-    "aoi": None,
-}
+with st.sidebar:
+    st.title("Filters")
+    min_date = df["scene_date"].min().to_pydatetime()
+    max_date = df["scene_date"].max().to_pydatetime()
+    slider = st.slider('Dates', min_value=min_date, max_value=max_date, value=[min_date, max_date])
+    cloud_cover = st.slider('Cloud cover', min_value=0, max_value=100, value=100)
+    na_cover = st.slider('No data cover', min_value=0, max_value=100, value=100)
+    valid_cover = st.slider('Valid cover', min_value=0, max_value=100, value=100)
+    sun_elevation = st.slider('Sun elevation', min_value=-90, max_value=90, value=(10, 90))
 
-st.session_state["error"] = {}
-st.session_state["output"] = {}
-st.session_state["output"]["today"] = today
+df["sclicing"] = (
+    (df["scene_date"] >= slider[0]) & (df["scene_date"] <= slider[1]) &
+    (df["cloud_cover"] <= cloud_cover) &
+    (df["no_data"] <= na_cover) &
+    (df["valid_pixel"] <= valid_cover) &
+    (df["sun_elevation"] >= sun_elevation[0]) & (df["sun_elevation"] <= sun_elevation[1])
+)
 
 
-# Some User text need to be checked if they have been filled
-def st_text_input_validated(key, **kwargs):
-    kwargs.update({"value": form_default[key]})
-    st.session_state["output"][key] = st.text_input(**kwargs)
-#     st.session_state["error"][key] = st.empty()
+# time_count = df.groupby(pd.Grouper(key='scene_datetime', freq='1d')).size().reset_index(name='count')
+# time_count["highlight"] = (time_count["scene_datetime"] >= slider[0]) & (time_count["scene_datetime"] <= slider[1])
 
-def is_kebab_case(string):
-    pattern = r'^[a-z][a-z0-9]*(?:-[a-z0-9]+)*$'
-    return re.match(pattern, string) is not None
+st.title("QC")
+st.header(f"{df['sclicing'].sum()}/{len(df)} Images")
 
-# suggestion
-# st_datalist
-
-st.header("Customer Request Form")
-
-# Client Information
-with st.expander("Client Information"):
-
-    # If existing client, select from the list, if not, input the name
-    new_client = st.radio(
-        label="Existing client",
-        options=["Yes", "No"],
-        index=0
+# Create the histogram with Altair
+histogram = alt.Chart(df).mark_bar().encode(
+    x=alt.X('scene_datetime:T', title=''),
+    y=alt.Y('count:Q', title=''),
+    color=alt.condition(
+        alt.datum.sclicing,
+        alt.value('#ff4242'),
+        alt.value('#343840')
     )
+).properties(height=60).configure_axisY(grid=False, labels=False)
 
-    if new_client == "No":
-        organisation = st.text_input(value=form_default["organisation"], label="Organisation Name")        
-    else:
-        organisation = st.selectbox("Organisation Name", options=customers, index=form_default["organisation"])
-    st.session_state["error"]["organisation"] = st.empty()
+st.altair_chart(histogram, use_container_width=True)
 
-    # Check for kebab-case format
-    if (organisation is None) or (organisation == ""):
-        pass
-    elif is_kebab_case(organisation):
-        st.session_state["output"]["organisation"] = organisation
-    else:
-        st.session_state["error"]["organisation"].error("Not 'kebab-case' format")
-    
-    # Contact Information
-    st.session_state["output"]["contact"] = st.text_input(value=form_default["contact"], label="Person of contact from the organisation")
-    st.session_state["error"]["contact"] = st.empty()
+df_sliced = df[df['sclicing']]
+event = st.dataframe(
+    df_sliced,
+    key=[0],
+    column_order=["rgb", "lst", "scene_datetime", "cloud_cover", "no_data", "sun_elevation", "atm_source"],
+    hide_index=True,
+    on_select="rerun",
+    selection_mode="multi-row",
+    column_config={
+        "rgb": st.column_config.ImageColumn(width="small"),
+        "lst": st.column_config.ImageColumn(width="small"),
+        }
+)
 
-    st.session_state["output"]["contract_type"] = st.radio(
-        label="Contract Type",
-        options=["EAP", "Single Order"],
-        index=form_default["contract_type"]
-    )
-    st.session_state["output"]["contract_status"] = st.radio(
-        label="Contract Status",
-        options=["Confirmed", "Pending"],
-        index=form_default["contract_status"]
-    )
-    st.session_state["output"]["paid_order"] = st.radio(
-        label="Paid Order",
-        options=["Yes", "No"],
-        index=form_default["paid_order"]
-    )
-    st.session_state["output"]["constellr_contact"] = st.text_input(value=form_default["constellr_contact"], label="Constellr Person of contact")
 
-# Order Information
-with st.expander("Order Information"):
-
-    st.session_state["output"]["order_type"] = st.radio(
-        label="Order Type",
-        options=["LST", "ET", "Analytics"],
-        index=form_default["order_type"]
-    )
-
-    st.session_state["output"]["requested_order"] = st.multiselect(
-        'Requested Order Type',
-        [
-            'Single Order',
-            'Watch Order'
-        ],
-        default=form_default["requested_order"]
-    )
-    st.session_state["output"]["requested_period"] = st.date_input(
-        "Requested period",
-        value=form_default["requested_period"], # Default range
-        min_value=date(2016, 1, 1), # Min date
-        max_value=today, # Max date
-        format="YYYY/MM/DD",
-    )
-    st.session_state["output"]["requested_delivery"] = st.date_input(
-        "Expected delivery date",
-        value=form_default["requested_delivery"], # Default date
-        min_value=today, # Min date
-        max_value=next_6_months, # Max date
-        format="YYYY/MM/DD",
-    )
-    st.session_state["output"]["delivery_method"] = st.radio(
-        label="Prefered delivery method",
-        options=["Download link", "API (not available yet)"],
-        index=form_default["delivery_method"]
-    )
-    st.session_state["output"]["comment"] = st.text_area("Comment", form_default["comment"])
-
-# AOI Information
-with st.expander("AOI Uploader"):
-    tab_draw, tab_upload, tab_uploads = st.tabs(["Draw AOI", "Upload Single AOI file (beta)", "Upload Multiple AOI files"])
-    
-    with tab_draw:
-        st.header("Draw AOI")
-        single_file = True
-        aoi = None
-
-        m_data = st_folium(draw_aoi_on_map(), use_container_width=True, height=500)
-        
-        if m_data["all_drawings"]:
-            aoi = gpd.GeoDataFrame.from_features(m_data["all_drawings"])
-
-    with tab_upload:
-        st.header("Upload Single AOI file")
-        st.markdown("Upload a single file, that may contain multiple AOIs.")
-        st.markdown("It allows for visaualisation/editing/preprocessing of the AOIs and minimize the operator work.")
-        uploaded_file = st.file_uploader("Upload GeoJSON:", type=["json", "geojson", "zip", "gpkg"])
-
-        if uploaded_file:
-            # Read AOI
-            single_file = True
-            aoi = None
-            aoi = read_uploaded_file(uploaded_file)
-
-            # Buffering
-            buffer = st.number_input("Buffer", min_value=0, max_value=10000, step=100)
-            if buffer > 0:
-                aoi = aoi.to_crs(aoi.estimate_utm_crs())
-                aoi.geometry = aoi.geometry.buffer(buffer, join_style=2)
-                aoi = aoi.to_crs(4326)
-
-            # Dissolving
-            col1, col2 = st.columns(2)
-            with col1:
-                is_dissolving = st.checkbox("Dissolving")
-            with col2:
-                if is_dissolving:
-                    dissolving = st.selectbox("Dissolving", [None] + list(aoi.columns))
-                    aoi = aoi.dissolve(dissolving)
-
-            st.header("Data Editing")
-            aoi = edit_table(aoi)
-
-            st.header("Dynamic map")
-            if aoi.shape[0] > 0:
-                st_folium(display_aoi_on_map(aoi), use_container_width=True, height=500)
-            else:
-                st.markdown("**:red[No shape to show]**")
-
-    with tab_uploads:
-        st.header("Upload Multiple AOI files")
-        st.markdown("Upload a multiple files, that may contain multiple AOIs.")
-        st.markdown("No pre-processing or visualization is available for this option, the preprocessing will be done by the operator.")
-        uploaded_file = st.file_uploader("Upload files:", accept_multiple_files=True)
-
-        if uploaded_file:
-            # Read AOI
-            single_file = False
-            aoi = uploaded_file.copy()
-            # for i in uploaded_file:
-            #     print(i.name)
-            #     # print(i.getvalue())
-
-            if aoi is None:
-                st.markdown("**:red[No file to upload]**")
-
-                
-
-submit = st.button("Submit")
-
-if submit:
-    # Checking that what needed to be answered has been answered
-    not_answered = []
-    
-    for key in st.session_state["error"].keys():
-        if st.session_state["output"][key] == form_default[key]:
-            not_answered.append(key)
-            st.session_state["error"][key].error("Required")
-    
-    if aoi is None:
-        not_answered.append("AOI")
-    elif len(aoi) == 0:
-        not_answered.append("AOI")
-
-    # In case there are no missing informations, the issue is created and aoi saved
-    if len(not_answered) > 0:
-        st.markdown(f"**:red[The following informations are missing {not_answered}]**")
-    else:
-        # Saving aoi
-        if single_file:
-            aoi_name = st.session_state["output"]["organisation"] + "_" + today.strftime("%F") + ".geojson"
-            aoi_path = f"s3://constellr-product-segment/form/{aoi_name}"
-            
-            with fsspec.open(aoi_path, "wb") as f:
-                aoi.to_file(f, driver="GeoJSON")
-
-            st.session_state["output"]["aoi"] = f"aws s3 cp {aoi_path} ."
-        else:
-            aoi_name = st.session_state["output"]["organisation"] + "_" + today.strftime("%F")
-            aoi_dir = f"s3://constellr-product-segment/form/{aoi_name}/"
-
-            for i in uploaded_file:
-                aoi_path = aoi_dir + i.name
-                with fsspec.open(aoi_path, "wb") as f:
-                    f.write(i.getvalue())
-                print("upload", aoi_path)
-            st.session_state["output"]["aoi"] = f"aws s3 cp --recursive {aoi_dir} ."
-
-        # Creating issue
-        title = format_title(st.session_state["output"])
-        body = format_body(st.session_state["output"])
-
-        update = form_to_github(st.session_state["output"])
-        issue_id = creating_issue(title, body)
-        issue_project_id = attributing_issue(issue_id)
-
-        st.markdown("**:green[Your request has been sent!]**")
+button = st.button("Done")
+if button:
+    st.write(f"You selected the following rows: {event.selection.rows}")
