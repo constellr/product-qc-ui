4f3cce3f80a6cee83e592631a043bdc16669940a
Jonas Berhin <jonas.berhin@constellr.com>
2024-08-02 13:43:12 +0200

    yes-viz-no slider

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ TODO:
 lst plots
 bigger images
 add tile column
+
+source .venv/bin/activate
+streamlit run src/main.py
--- src/main.py
+++ src/main.py
@@ -18,11 +18,11 @@ def parse_json(path):
     metadata = json.load(open(f"{path}/metadata.json"))
     metadata["scene_datetime"] = pd.Timestamp(metadata["scene_datetime"])
     metadata["scene_date"] = pd.Timestamp(metadata["scene_datetime"]).normalize()
-    metadata["valid_pixel"] = 100 - metadata["cloud_cover"] - metadata["no_data"]
+    metadata["invalid_pixel"] = metadata["cloud_cover"] + metadata["no_data"]
     metadata["rgb"] = open_image(f"{path}/rgb.png")
     metadata["lst"] = open_image(f"{path}/lst.png")
     metadata["count"] = 1
-    metadata["sclicing"] = True
+    metadata["viz"] = True
     metadata["colors"] = 1
     return metadata
 
@@ -34,7 +34,8 @@ def timeline():
         y=alt.Y('count:Q', title=''),
         color=alt.Color('colors', scale=alt.Scale(
             domain=[0, 1, 2],
-            range=['#343840', '#ffc2c2', "#ff4242"]
+            # range=['#343840', '#ffc2c2', "#ff4242"]
+            range=['lime', 'orange', 'red']
             ),
         legend=None
             )
@@ -42,12 +43,13 @@ def timeline():
     
 
 def ts():
-    return alt.Chart(st.session_state.df).mark_circle(size=60).encode(
+    return alt.Chart(st.session_state.df).mark_circle(size=300).encode(
         x=alt.X('scene_datetime:T', title=''),
         y=alt.Y('lst_median:Q', title='LST Median'),
         color=alt.Color('colors:N', scale=alt.Scale(
             domain=[0, 1, 2],
-            range=['#343840', '#ffc2c2', '#ff4242']
+            # range=['#343840', '#ffc2c2', '#ff4242']
+            range=['red', 'lime', 'orange', ]
             ),
             legend=None
         )
@@ -57,37 +59,45 @@ def ts():
         )
 
 
-
-
 @st.cache_data
 def load():
     df = df = pd.DataFrame([parse_json(d) for d in glob("tests/fixtures/*")])
     min_date = df["scene_date"].min().to_pydatetime()
     max_date = df["scene_date"].max().to_pydatetime()
-    selected = []
-    df_sliced = df[df['sclicing']]
-    return df, min_date, max_date, selected, df_sliced
+    df_viz_selected = []
+    df_viz = df[df['viz']]
+    return df, min_date, max_date, df_viz_selected, df_viz
 
-st.session_state.df, st.session_state.min_date, st.session_state.max_date, st.session_state.selected, st.session_state.df_sliced = load()
+st.session_state.df, st.session_state.min_date, st.session_state.max_date, st.session_state.df_viz_selected, st.session_state.df_viz = load()
 
 # Sidebar
 with st.sidebar:
     st.title("Filters")
     date_slider = st.slider('Dates', min_value=st.session_state.min_date, max_value=st.session_state.max_date, value=[st.session_state.min_date, st.session_state.max_date])
-    cloud_cover = st.slider('Cloud cover', min_value=0, max_value=100, value=100)
-    na_cover = st.slider('No data cover', min_value=0, max_value=100, value=100)
-    valid_cover = st.slider('Valid cover', min_value=0, max_value=100, value=100)
+    cloud_cover = st.slider('Cloud cover', min_value=0, max_value=100, value=(0, 100))
+    na_cover = st.slider('No data cover', min_value=0, max_value=100, value=(0, 100))
+    invalid_cover = st.slider('Invalid cover', min_value=0, max_value=100, value=(0, 100))
     sun_elevation = st.slider('Sun elevation', min_value=-90, max_value=90, value=(10, 90))
 
     # Updating df_sliced
-    st.session_state.df["sclicing"] = (
+    st.session_state.df["viz"] = (
         (st.session_state.df["scene_date"] >= date_slider[0]) & (st.session_state.df["scene_date"] <= date_slider[1]) &
-        (st.session_state.df["cloud_cover"] <= cloud_cover) &
-        (st.session_state.df["no_data"] <= na_cover) &
-        (st.session_state.df["valid_pixel"] <= valid_cover) &
+        (st.session_state.df["cloud_cover"] >= cloud_cover[0]) & (st.session_state.df["cloud_cover"] <= cloud_cover[1]) &
+        (st.session_state.df["no_data"] >= na_cover[0]) & (st.session_state.df["no_data"] <= na_cover[1]) &
+        (st.session_state.df["invalid_pixel"] >= invalid_cover[0]) & (st.session_state.df["invalid_pixel"] <= invalid_cover[1]) &
         (st.session_state.df["sun_elevation"] >= sun_elevation[0]) & (st.session_state.df["sun_elevation"] <= sun_elevation[1])
     )
-    st.session_state.df_sliced = st.session_state.df[st.session_state.df['sclicing']]
+
+    st.session_state.df["default_selected"] = (
+        (st.session_state.df["scene_date"] >= date_slider[0]) & (st.session_state.df["scene_date"] <= date_slider[1]) &
+        (st.session_state.df["cloud_cover"] <= cloud_cover[1]) &
+        (st.session_state.df["no_data"] <= na_cover[1]) &
+        (st.session_state.df["invalid_pixel"] <= invalid_cover[1]) &
+        (st.session_state.df["sun_elevation"] >= sun_elevation[0]) & (st.session_state.df["sun_elevation"] <= sun_elevation[1])
+    ).astype(int)
+    st.session_state.df["default_selected"][st.session_state.df['viz']] = 2
+
+    st.session_state.df_viz = st.session_state.df[st.session_state.df['viz']]
 
 
 # Main panel
@@ -95,7 +105,7 @@ st.title("QC")
 header = st.container()
 
 event = st.dataframe(
-    st.session_state.df_sliced,
+    st.session_state.df_viz,
     key=[0],
     column_order=["rgb", "lst", "scene_datetime", "cloud_cover", "no_data", "sun_elevation", "atm_source"],
     hide_index=True,
@@ -108,15 +118,17 @@ event = st.dataframe(
 )
 
 # Slicing selected rows
-st.session_state.df_selected = st.session_state.df_sliced.iloc[event.selection.rows]
-st.session_state.selected = st.session_state.df_selected.index.tolist()
+st.session_state.df_viz_selected = st.session_state.df_viz.iloc[event.selection.rows]
+st.session_state.selected = st.session_state.df_viz_selected.index.tolist()
 
 # Header - image count update
-header.header(f"{len(st.session_state.df_selected)}/{len(st.session_state.df)} Images")
+header.header(f"{len(st.session_state.df_viz_selected)}/{len(st.session_state.df)} Images")
 
 # Header - timeline update
-st.session_state.df["colors"] = st.session_state.df["sclicing"].astype(int)
-st.session_state.df.loc[st.session_state.selected, "colors"] = 2
+st.session_state.df["colors"] = st.session_state.df["default_selected"]
+
+st.session_state.df.loc[st.session_state.selected, "colors"] = 1
+print(st.session_state.df["colors"])
 # header.altair_chart(timeline(), use_container_width=True)
 header.altair_chart(ts(), use_container_width=True)
 
